1e48b8ff4f0aba11f8e092b0fb63e3e0385b08b1
ballwr <ballwr@miamioh.edu>
2022-11-04 15:51:21 -0400

    Added add ball example to sqlExample.js

--- sqlExample.js
+++ sqlExample.js
@@ -7,7 +7,7 @@ let procedure = `CALL addUser(?, ?)`;
 let name = "phil";
 let password = "abc123"
 
-
+// This is for adding a user
 connection.query(procedure, [name, password], function (err, result) {
   if (err) {
     return console.error(err.message);
@@ -15,4 +15,12 @@ connection.query(procedure, [name, password], function (err, result) {
   console.log("User added");
 });
 
+
+// This is for adding a ball 
+connection.query('Call addBall(?, ?, ?, ?)', [1, value.name, value.weight, value.color], function (err, result) {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log("Ball added")
+});
 connection.end();
