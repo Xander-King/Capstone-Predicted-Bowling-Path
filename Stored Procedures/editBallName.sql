304ea4fcac9b1177361198ffca5903235bbd0633
ballwr <ballwr@miamioh.edu>
2023-02-14 18:18:59 -0500

    Fixed editBallName

--- Stored Procedures/editBallName.sql
+++ Stored Procedures/editBallName.sql
@@ -1,8 +1,8 @@
 DELIMITER //
 
-CREATE PROCEDURE editUserName (IN p_userId INT, p_name varchar(45)) 
+CREATE PROCEDURE editBallName (IN p_ballId INT, p_name varchar(45)) 
 BEGIN
-	UPDATE 	bowlingapp.users
-    SET users.userName = p_name
-    WHERE users.userId = p_userId;
+	UPDATE 	bowlingapp.balls
+    SET balls.ballName = p_name
+    WHERE balls.ballId = p_ballId;
 END
